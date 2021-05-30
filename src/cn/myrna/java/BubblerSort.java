df81ad5e43212454f7e3d4cedbaf134dc4ad18bb
Mrna0825 <1463673355@qq.com>
2021-05-30 23:05:28 +0800

    Learn java from scratch

---
+++ src/cn/myrna/java/BubblerSort.java
@@ -0,0 +1,25 @@
+package cn.myrna.java;
+
+import java.util.Arrays;
+
+public class BubblerSort {
+    public static void bubblerSort(int[] arr){
+            int temp;
+            for(int i = 0; i<arr.length-1;i++){
+                for (int j = 0; j <arr.length-i-1; j++) {
+                    if(arr[j+1]<arr[j]){
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1]  = temp;
+                    }
+                }
+            }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = new int[]{3,15,6,7,88,9,1,2,8};
+        BubblerSort.bubblerSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ src/cn/myrna/java/test.java
@@ -0,0 +1,41 @@
+package cn.myrna.java;
+
+public class test {
+
+    public static void printLn_Method(){
+        for (int i = 0; i < 5; i++) {
+
+            for (int i1 = 0; i1 < 20; i1++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static int method_sum(int a,int b){
+
+        int result = a + b;
+
+        return result;
+    }
+
+
+    public static void addition1_100(){
+        int sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+
+    public static void main(String[] args) {
+        //printLn_Method();
+        //System.out.println(method_sum(1,2));
+        //int num = method_sum(5,5);
+        //System.out.println(num);
+        addition1_100();
+
+    }
+}
